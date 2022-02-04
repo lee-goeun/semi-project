0baf9b5df5da89d87755a8075d8150e885ac165c
kimsanhaa <ttksgk@gmail.com>
2022-02-04 20:37:34 +0900

    test

--- src/main/java/com/mycompany/myapp/controller/MemberController.java
+++ src/main/java/com/mycompany/myapp/controller/MemberController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+ 
 import com.mycompany.myapp.service.MemberService;
 import com.mycompany.myapp.vo.MemberVO;
 
